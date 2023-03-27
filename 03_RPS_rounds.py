00f33f3d6df1d3be9ed08c68a909c8f8df7bb7fb
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2023-03-28 10:41:07 +1300

    Add files via upload

--- 03_RPS_rounds.py
+++ 03_RPS_rounds.py
@@ -26,6 +26,29 @@ rounds = check_rounds()
 
 end_game = "no"
 while end_game == "no":
+
+    # Start of gameplay loop
+
+    # rounds heading
     print()
     if rounds == "":
-        heading = f"Continuous mode: Round {rounds_played + 1}"
+        heading = f"Continuous mode: [Round #{rounds_played + 1}]"
+
+    else:
+        heading = f"[Round #{rounds_played + 1} of {rounds}]"
+
+    print(heading)
+    choose = input(f"{choose_instruction} or 'xxx' to end: ")
+
+    if choose == "xxx":
+        break
+
+    if rounds_played == rounds - 1:
+        end_game = "yes"
+
+    print(f"you chose {choose}")
+
+    rounds_played += 1
+
+print()
+print("thank you for playing")
